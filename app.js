fef0d164e8c8e2f180d0073408460fb4a63c43ac
Hijiri Tamaki <>
2020-01-30 18:29:50 +0800

    プルダウンメニューとスムーススクロール実装

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-$('.left')
-.on("mouseover",function(){
-    $('#sideber').slideToggle();
-})
-$('#header-left')
-.on("mouseleave",function(){
-    $('#sideber').slideToggle();
-})
 
 
-// $('a[href^=#]').click(function(){
-//       var adjust = 20;
-//       var speed = 10;
-//       var href= $(this).attr("href");
-//       var target = $(href == "#" || href == "" ? 'html' : href);
-//       var position = target.offset().top + adjust;
-//       $('body,html').animate({scrollTop:position}, speed, 'swing');
-//       return false;
-// });
+$(function(){
+    $('.dropdwn li').hover(function(){
+        $("ul:not(:animated)", this).slideDown();
+    }, function(){
+        $("ul.dropdwn_menu",this).slideUp();
+    });
+});
+
+
 
 $('.hoge').on('click', function() {
     href= $(this).attr("href");
 
-    let target = $(href).offset().top;
+ let target = $(href).offset().top;
 
-    $('html, body').animate({scrollTop: target}, 500);
+  $('html, body').animate({scrollTop: target}, 500);
 
-})  
+})
+
