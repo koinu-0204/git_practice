74afc095c2caf1feeb5b5f10d5c5d72fcb7e8bd1
Hijiri Tamaki <>
2020-01-31 10:26:55 +0800

    ちょっとした修正

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ $(window).scroll(function(){
 
 $(function(){
     var pagetop = $('#page_top');
-    // ボタン非表示
+    
     pagetop.hide();
-    // 100px スクロールしたらボタン表示
+    
     $(window).scroll(function () {
        if ($(this).scrollTop() > 100) {
             pagetop.fadeIn();
